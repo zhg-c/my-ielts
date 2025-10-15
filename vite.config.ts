91747bdff1288e2f3bde0f580961cc2680563958
zhg <1640532428@qq.com>
2025-10-15 13:23:32 +0800

    configure it to run. deploy gh-pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+const REPO_NAME = 'my-ielts';
+
+// 使用回调函数来获取命令类型
+export default defineConfig(({ command }) => {
+  // 只有在执行 'build' 命令时，才将 base 设置为子路径
+  const isProd = command === 'build';
+
+  return {
+    plugins: [vue()],
+    
+    // 如果是 build，base 为 /my-ielts/，否则为 /
+    base: isProd ? `/${REPO_NAME}/` : '/',
+  }
 })
